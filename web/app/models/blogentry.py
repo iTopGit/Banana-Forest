82fd3f98c0917ad2adab1833dac2460a9809845a
SaCHiMiJi <junjao800@gmail.com>
2023-03-17 02:51:35 +0700

    edit canvas color and msg hide and add on like function

--- web/app/models/blogentry.py
+++ web/app/models/blogentry.py
@@ -8,12 +8,14 @@ class BlogEntry(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String(280))
     img_id = db.Column(db.String(50))
+    like = db.Column(db.JSON, nullable=True)
     date_created = db.Column(db.DateTime)
     date_updated = db.Column(db.DateTime)
     
-    def __init__(self, email, img_id):
-        self.email = email
+    def __init__(self, message, img_id, like=None):
+        self.message = message
         self.img_id = img_id
+        self.like = like or []
         self.date_created = datetime.datetime.now()
         self.date_updated = datetime.datetime.now()
 
@@ -21,3 +23,9 @@ class BlogEntry(db.Model, SerializerMixin):
         self.message = message
         self.img_id = img_id
         self.date_updated = datetime.datetime.now()
+        
+    def like_update(self, like):
+        self.like += like
+        
+    def like_remove(self, item):
+        self.like.remove(item)
