263b12813592fe455209d8c662be5278851b6e59
LunaTearR <saengtawanpoopoom@gmail.com>
2023-03-17 13:54:15 +0700

    edit-manage

--- web/manage.py
+++ web/manage.py
@@ -2,9 +2,8 @@ from flask.cli import FlaskGroup
 from werkzeug.security import generate_password_hash
 import datetime
 from app import app, db
-from app.models.contact import Contact
 from app.models.blogentry import BlogEntry
-from app.models.authuser import AuthUser
+from app.models.authuser import AuthUser, PosonalPost
 
 cli = FlaskGroup(app)
 
@@ -18,22 +17,21 @@ def create_db():
 
 @cli.command("seed_db")
 def seed_db():
-    db.session.add(
-        BlogEntry(message='musu', email='capybara@slay.ca.py', img_id='-1',avatar_url='https://ui-avatars.com/api/?name=\สมชาย+ทรงแบด&background=83ee03&color=fff')
-    )
-    db.session.commit()
-    
-    db.session.add(AuthUser(email="flask@204212", name='สมชาย ทรงแบด',
+    db.session.add(AuthUser(email="capybara@mail.com", name='capybara',
         password=generate_password_hash('1234', method='sha256'),
-        avatar_url='https://ui-avatars.com/api/?name=\สมชาย+ทรงแบด&background=83ee03&color=fff'))
+        avatar_url='../static/img/capi.jpg'))
+    db.session.commit()
+    db.session.add(
+        PosonalPost(owner_id=1,message='musu', img_id='-1')
+    )
     db.session.commit()
     db.session.add(AuthUser(email="cap@suit", name='suit',
         password=generate_password_hash('1234', method='sha256'),
-        avatar_url='static\img\capybara_suit.jpg'))
+        avatar_url='../static/img/capybara_suit.jpg'))
     db.session.commit()
-    db.session.add(AuthUser(email="cap@smile", name='smile',
-        password=generate_password_hash('1234', method='sha256'),
-        avatar_url='static\img\capybara_smile.jpg'))
+    db.session.add(
+        PosonalPost(owner_id=2,message='capybara', img_id='-1')
+    )
     db.session.commit()
 
 
